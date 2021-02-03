b1311daa20914026e617966be8fb25b2f972c7fb
Lucas Lopes <lucaslopesnerd@gmail.com>
2021-02-03 15:36:25 -0300

    Estudos Dia 3

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4999.99
+prod1['Desconto Legal'] = 0.40 //Evitar usar com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.99
+}
+
+console.log(prod2)
+
